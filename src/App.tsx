b5e9f670d04d5c9adef29d4e5b05b8430bc429dc
Israel Kusayev <israel.kusayev@gmail.com>
2020-02-23 12:47:39 +0200

    :heavy_plus_sign: added antd v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Routes from './components/router/routes';
 import { Router } from 'react-router-dom';
 import history from './utils/history';
 import ErrorBoundary from 'components/errorBoundaries/errorBoundary';
+import { ConfigProvider } from 'antd';
+import heIL from 'antd/es/locale/he_IL';
+import moment from 'moment';
+import 'moment/locale/he';
+
+moment.locale('he');
 
 const App: React.FC = () => {
+  const [locale] = useState(heIL);
   return (
-    <React.StrictMode>
+    <ConfigProvider direction='rtl' locale={locale}>
       <ErrorBoundary>
         <Router history={history}>
           <Routes></Routes>
         </Router>
       </ErrorBoundary>
-    </React.StrictMode>
+    </ConfigProvider>
   );
 };
 
