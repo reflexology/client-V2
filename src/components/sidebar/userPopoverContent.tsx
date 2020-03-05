dd6c023dbb92914e53296a31958d8ed186a884a0
Israel Kusayev <israel.kusayev@gmail.com>
2020-03-06 00:31:43 +0200

    :sparkles: handle logout

--- src/components/sidebar/userPopoverContent.tsx
+++ src/components/sidebar/userPopoverContent.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Dictionary from 'dictionary/dictionary';
 import { LogoutOutlined } from '@ant-design/icons';
+import AuthService from 'services/authService';
+import { useHistory } from 'react-router-dom';
+import { routes } from 'components/router/routes';
 
 interface Props {}
 
 const UserPopoverContent: React.FC<Props> = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    AuthService.removeTokens();
+    history.push(routes.login);
+  };
+
   return (
     <div className='user-popover-content'>
-      <button className='popover-button'>
+      <button onClick={handleLogout} className='popover-button'>
         <LogoutOutlined rotate={180} />
         <span className='popover-text'>{Dictionary.sidebar.logout}</span>
       </button>
