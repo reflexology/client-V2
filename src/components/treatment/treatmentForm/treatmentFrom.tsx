a36e487263518b66e30ceab99c71ca390ca58afc
Israel Kusayev <israel.kusayev@gmail.com>
2020-04-28 20:28:31 +0300

    :lipstick: treatment form replace placeholder with label

--- src/components/treatment/treatmentForm/treatmentFrom.tsx
+++ src/components/treatment/treatmentForm/treatmentFrom.tsx
@@ -32,24 +32,50 @@ const TreatmentFrom: React.FC<TreatmentFromProps> = props => {
     props.onSubmit(values, newDiagnoses);
   };
 
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 5 }
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 19 }
+    }
+  };
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 24,
+        offset: 0
+      },
+      sm: {
+        span: 16,
+        offset: 8
+      }
+    }
+  };
+
   return (
-    <Form form={form} initialValues={props.initialValues} onFinish={onSubmit}>
-      <Form.Item name='treatmentDate'>
-        <DatePicker
-          showTime
-          format='DD/MM/YYYY HH:mm'
-          placeholder={Dictionary.treatmentForm.treatmentDate}
-          defaultValue={moment()}
-        />
+    <Form {...formItemLayout} form={form} initialValues={props.initialValues} onFinish={onSubmit}>
+      <Form.Item name='treatmentDate' label={Dictionary.treatmentForm.treatmentDate}>
+        <DatePicker showTime format='DD/MM/YYYY HH:mm' defaultValue={moment()} />
       </Form.Item>
-      <Form.Item name='referredBy' hasFeedback>
-        <Input autoComplete='off' placeholder={Dictionary.treatmentForm.referredBy} />
+      <Form.Item
+        // style={{ display: 'inline-flex' }}
+        name='treatmentNumber'
+        label={Dictionary.treatmentForm.treatmentNumber}
+        hasFeedback
+      >
+        <InputNumber style={{ width: '100%' }} autoComplete='off' />
       </Form.Item>
-      <Form.Item name='visitReason' hasFeedback>
-        <TextArea autoSize autoComplete='off' placeholder={Dictionary.treatmentForm.visitReason} />
+      <Form.Item name='referredBy' label={Dictionary.treatmentForm.referredBy} hasFeedback>
+        <Input autoComplete='off' />
       </Form.Item>
-      <Form.Item name='diagnoses' hasFeedback>
-        <Select showArrow loading={!diagnoses} placeholder={Dictionary.treatmentForm.diagnoses} mode='tags'>
+      <Form.Item name='visitReason' label={Dictionary.treatmentForm.visitReason} hasFeedback>
+        <TextArea autoSize autoComplete='off' />
+      </Form.Item>
+      <Form.Item name='diagnoses' label={Dictionary.treatmentForm.diagnoses} hasFeedback>
+        <Select showArrow loading={!diagnoses} mode='tags'>
           {diagnoses?.map(diagnosis => (
             <Select.Option value={diagnosis} key={diagnosis}>
               {diagnosis}
@@ -57,41 +83,39 @@ const TreatmentFrom: React.FC<TreatmentFromProps> = props => {
           ))}
         </Select>
       </Form.Item>
-      <Form.Item name='findings' hasFeedback>
-        <TextArea autoSize autoComplete='off' placeholder={Dictionary.treatmentForm.findings} />
+      <Form.Item name='findings' label={Dictionary.treatmentForm.findings} hasFeedback>
+        <TextArea autoSize autoComplete='off' />
       </Form.Item>
-      <Form.Item name='recommendations' hasFeedback>
-        <TextArea autoSize autoComplete='off' placeholder={Dictionary.treatmentForm.recommendations} />
+      <Form.Item name='recommendations' label={Dictionary.treatmentForm.recommendations} hasFeedback>
+        <TextArea autoSize autoComplete='off' />
       </Form.Item>
-      <Row justify='space-between'>
-        <Form.Item style={{ display: 'inline-block' }} name='treatmentNumber' hasFeedback>
-          <InputNumber
-            style={{ width: '100%' }}
-            autoComplete='off'
-            placeholder={Dictionary.treatmentForm.treatmentNumber}
-          />
-        </Form.Item>
-        <Form.Item
-          style={{ display: 'inline-block' }}
-          name='treatmentPrice'
-          extra={Dictionary.treatmentForm.treatmentPriceExtra.format(
-            props.balance >= 0 ? Dictionary.treatmentForm.credit : Dictionary.treatmentForm.debt,
-            Math.abs(props.balance).toString()
-          )}
-          hasFeedback
-        >
-          <InputNumber
-            style={{ width: '100%' }}
-            autoComplete='off'
-            placeholder={Dictionary.treatmentForm.treatmentPrice}
-          />
-        </Form.Item>
-        <Form.Item rules={[{ type: 'number' }]} style={{ display: 'inline-block' }} name='paidPrice' hasFeedback>
-          <InputNumber style={{ width: '100%' }} autoComplete='off' placeholder={Dictionary.treatmentForm.paidPrice} />
-        </Form.Item>
-      </Row>
-      <Form.Item name='reminders' hasFeedback>
-        <TextArea autoSize autoComplete='off' placeholder={Dictionary.treatmentForm.reminders} />
+      <Form.Item
+        style={{ display: 'inline-flex', width: '50%' }}
+        wrapperCol={{ span: 14 }}
+        labelCol={{ span: 10 }}
+        name='treatmentPrice'
+        extra={Dictionary.treatmentForm.treatmentPriceExtra.format(
+          props.balance >= 0 ? Dictionary.treatmentForm.credit : Dictionary.treatmentForm.debt,
+          Math.abs(props.balance).toString()
+        )}
+        hasFeedback
+        label={Dictionary.treatmentForm.treatmentPrice}
+      >
+        <InputNumber style={{ width: '100%' }} autoComplete='off' />
+      </Form.Item>
+      <Form.Item
+        style={{ display: 'inline-flex', width: '50%' }}
+        wrapperCol={{ span: 13 }}
+        labelCol={{ span: 9, offset: 2 }}
+        rules={[{ type: 'number' }]}
+        name='paidPrice'
+        hasFeedback
+        label={Dictionary.treatmentForm.paidPrice}
+      >
+        <InputNumber style={{ width: '100%' }} autoComplete='off' />
+      </Form.Item>
+      <Form.Item name='reminders' hasFeedback label={Dictionary.treatmentForm.reminders}>
+        <TextArea autoSize autoComplete='off' />
       </Form.Item>
       <Form.Item name='reminderDate' label={Dictionary.treatmentForm.reminderDate}>
         <DatePicker
@@ -108,7 +132,7 @@ const TreatmentFrom: React.FC<TreatmentFromProps> = props => {
       </Form.Item>
       ,{props.error && <Alert message={props.error} type='error' showIcon />}
       <Form.Item>
-        <Button block loading={props.isLoading} type='primary' htmlType='submit'>
+        <Button {...tailFormItemLayout} block loading={props.isLoading} type='primary' htmlType='submit'>
           {Dictionary.patientForm.save}
         </Button>
       </Form.Item>
