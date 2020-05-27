2351911527eec3aa3323bc0bf530739fe9e0abad
Sanad Satel <sanadsatel@gmail.com>
2020-05-27 19:41:10 +0300

    Update src/components/patient/addPatient/addPatient.tsx
    Co-authored-by: Israel kusayev <israel.kusayev@gmail.com>

--- src/components/patient/addPatient/addPatient.tsx
+++ src/components/patient/addPatient/addPatient.tsx
@@ -24,7 +24,7 @@ const AddPatient: React.FC<Props> = props => {
     setIsSubmitting(true);
     setError('');
 
-    values.childrenAges = values.childrenAges.filter((e: number) => e);
+    values.childrenAges = values.childrenAges?.filter((childAge: number) => !!childAge);
     PatientService.addPatient(values)
       .then(patient => {
         if (buttonClicked === Dictionary.patientForm.saveAndAddTreatment) {
