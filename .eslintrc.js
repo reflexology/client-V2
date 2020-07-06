11d544f393c346f16adde4b1510e3df91f141720
Israel kusayev <israel.kusayev@gmail.com>
2020-07-06 13:22:40 +0300

    :wrench: updating import sort config

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const folders = fs
+  .readdirSync('src', { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name);
+
+module.exports = {
+  extends: ['react-app', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'react-hooks/exhaustive-deps': 'off',
+    'no-extend-native': 'off',
+    'simple-import-sort/sort': 'error',
+    'sort-imports': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error'
+  },
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts'],
+      rules: {
+        'simple-import-sort/sort': [
+          'error',
+          {
+            groups: [
+              // Packages. `react` related packages come first.
+              // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+              ['^react', '^@?\\w'],
+              // Absolute imports and Relative imports.
+              [`^(${folders.join('|')})(/.*|$)`, '^\\.'],
+              // for scss imports.
+              ['^[^.]']
+            ]
+          }
+        ]
+      }
+    }
+  ]
+};
