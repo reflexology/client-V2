8e2cdf3745385466ea64ae847c7e6942681de7c9
Israel kusayev <israel.kusayev@gmail.com>
2020-07-24 12:21:17 +0300

    :sparkles: reminders list

--- src/components/topbar/reminders.tsx
+++ src/components/topbar/reminders.tsx
@@ -3,41 +3,73 @@ import { BellOutlined } from '@ant-design/icons';
 import { Badge, List, Tabs, Spin } from 'antd';
 import HeaderDropdown from '../headerDropdown/headerDropdown';
 import ReminderService, { Reminder } from 'services/reminderService';
+import Dictionary from 'dictionary/dictionary';
+import { useHistory } from 'react-router-dom';
+import { routes } from 'components/router/routes';
+import { DATE_FORMAT } from 'utils/constants';
+import moment from 'moment';
 
 interface IRemindersProps {}
 
-type reminderType = 'all' | 'new';
+enum ReminderType {
+  All = 'all',
+  New = 'new'
+}
 
 const Reminders: React.FC<IRemindersProps> = () => {
   const [visible, setVisible] = useState(false);
-  const [showNew, setShowNew] = useState(true);
+  const [remindersType, setRemindersType] = useState(ReminderType.New);
   const [reminders, setReminders] = useState<Reminder[]>([]);
   const [isFetching, setIsFetching] = useState(true);
 
+  const history = useHistory();
+
   useEffect(() => {
-    ReminderService.getReminders(showNew)
+    ReminderService.getReminders(remindersType === ReminderType.New)
       .then(setReminders)
       .finally(() => setIsFetching(false));
-  }, [showNew]);
+  }, [remindersType]);
 
-  const changeShowNewReminders = (key: string) => setShowNew(key === 'new');
+  const handleItemClick = (reminder: Reminder) => {
+    history.push(routes.treatment.format(reminder.treatmentId));
+    setVisible(false);
+  };
 
   const remindersMenu = (
-    <List>
-      {reminders.map(r => {
-        return <List.Item key={r.treatmentId}>{r.reminders}</List.Item>;
-      })}
+    <List className='list'>
+      {reminders.map(reminder => (
+        <List.Item onClick={() => handleItemClick(reminder)} className='item' key={reminder.treatmentId}>
+          <List.Item.Meta
+            className='meta'
+            title={<div className='title'>{reminder.firstName + ' ' + reminder.lastName}</div>}
+            description={
+              <div>
+                <div className='description'>{reminder.reminders}</div>
+                <div className='date'>{moment(reminder.reminderDate).format(DATE_FORMAT)}</div>
+              </div>
+            }
+          />
+        </List.Item>
+      ))}
     </List>
   );
+
+  const renderRemindersCount = (tab: ReminderType) => (tab === remindersType ? ` (${reminders.length})` : '');
 
   const getNotificationBox = () => {
     return (
       <Spin spinning={isFetching} delay={300}>
-        <Tabs centered defaultActiveKey='1' onChange={changeShowNewReminders} className='reminders-tabs'>
-          <Tabs.TabPane tab='תזכורות חדשות' key='all'>
+        <Tabs centered onChange={key => setRemindersType(key as ReminderType)} className='reminders-tabs'>
+          <Tabs.TabPane
+            tab={Dictionary.reminders.showNew + renderRemindersCount(ReminderType.New)}
+            key={ReminderType.New}
+          >
             {remindersMenu}
           </Tabs.TabPane>
-          <Tabs.TabPane tab='כל התזכורות' key='new'>
+          <Tabs.TabPane
+            tab={Dictionary.reminders.showAll + renderRemindersCount(ReminderType.All)}
+            key={ReminderType.All}
+          >
             {remindersMenu}
           </Tabs.TabPane>
         </Tabs>
