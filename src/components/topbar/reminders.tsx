0e24d242f701b6f7a5c1f9b2ff9c49edfc1e38a8
sanadsa <sanad_s96@hotmail.com>
2020-07-28 14:45:52 +0300

    :zap: Add react virtualized to remiders list

--- src/components/topbar/reminders.tsx
+++ src/components/topbar/reminders.tsx
@@ -7,6 +7,7 @@ import Dictionary from 'dictionary/dictionary';
 import { useHistory } from 'react-router-dom';
 import { routes } from 'components/router/routes';
 import { DATE_FORMAT } from 'utils/constants';
+import { AutoSizer, List as VList, CellMeasurer, CellMeasurerCache } from 'react-virtualized';
 import moment from 'moment';
 
 interface IRemindersProps {}
@@ -21,6 +22,12 @@ const Reminders: React.FC<IRemindersProps> = () => {
   const [remindersType, setRemindersType] = useState(ReminderType.New);
   const [reminders, setReminders] = useState<Reminder[]>([]);
   const [isFetching, setIsFetching] = useState(true);
+  const cache = React.useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100
+    })
+  );
 
   const history = useHistory();
 
@@ -36,22 +43,39 @@ const Reminders: React.FC<IRemindersProps> = () => {
   };
 
   const remindersMenu = (
-    <List className='list'>
-      {reminders.map(reminder => (
-        <List.Item onClick={() => handleItemClick(reminder)} className='item' key={reminder.treatmentId}>
-          <List.Item.Meta
-            className='meta'
-            title={<div className='title'>{reminder.firstName + ' ' + reminder.lastName}</div>}
-            description={
-              <div>
-                <div className='description'>{reminder.reminders}</div>
-                <div className='date'>{moment(reminder.reminderDate).format(DATE_FORMAT)}</div>
-              </div>
-            }
+    <div style={{ width: '100%', height: '100vh' }}>
+      <AutoSizer>
+        {({ width, height }) => (
+          <VList
+            width={width}
+            height={height}
+            rowHeight={cache.current.rowHeight}
+            deferredMeasurementCache={cache.current}
+            rowCount={reminders.length}
+            rowRenderer={({ key, index, style, parent }) => {
+              const reminder = reminders[index];
+
+              return (
+                <CellMeasurer key={key} cache={cache.current} parent={parent} columnIndex={0} rowIndex={index}>
+                  <List.Item onClick={() => handleItemClick(reminder)} className='item' style={style}>
+                    <List.Item.Meta
+                      className='meta'
+                      title={<div className='title'>{reminder.firstName + ' ' + reminder.lastName}</div>}
+                      description={
+                        <div>
+                          <div className='description'>{reminder.reminders}</div>
+                          <div className='date'>{moment(reminder.reminderDate).format(DATE_FORMAT)}</div>
+                        </div>
+                      }
+                    />
+                  </List.Item>
+                </CellMeasurer>
+              );
+            }}
           />
-        </List.Item>
-      ))}
-    </List>
+        )}
+      </AutoSizer>
+    </div>
   );
 
   const renderRemindersCount = (tab: ReminderType) => (tab === remindersType ? ` (${reminders.length})` : '');
@@ -64,13 +88,13 @@ const Reminders: React.FC<IRemindersProps> = () => {
             tab={Dictionary.reminders.showNew + renderRemindersCount(ReminderType.New)}
             key={ReminderType.New}
           >
-            {remindersMenu}
+            <List>{remindersMenu}</List>
           </Tabs.TabPane>
           <Tabs.TabPane
             tab={Dictionary.reminders.showAll + renderRemindersCount(ReminderType.All)}
             key={ReminderType.All}
           >
-            {remindersMenu}
+            <List>{remindersMenu}</List>
           </Tabs.TabPane>
         </Tabs>
       </Spin>
