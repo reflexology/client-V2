7a024f5b865a0ffe52d9f67ffcf01dea7b94201b
Israel kusayev <israel.kusayev@gmail.com>
2020-12-18 10:18:14 +0200

    ➖ remove storybook

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from '@storybook/react';
-import centered from '@storybook/addon-centered/react';
-import 'loaders.css/src/animations/ball-pulse.scss';
-import '../src/styles/main.scss';
-
-addDecorator(centered);
-
-// automatically import all files ending in *.stories.tsx
-configure(require.context('../stories', true, /\.stories\.tsx?$/), module);
